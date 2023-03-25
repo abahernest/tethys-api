2c62e648525dd5d44a46864a32449abdf68da887
Ernest Abah <abahernesto@gmail.com>
2023-03-25 03:16:49 +0100

    added appname as required field in request header

--- index.js
+++ index.js
@@ -14,25 +14,29 @@ const port = 3000;
 async function verifyAuthToken(req, res, next) {
     try{
         const md5_device_hash = req.headers.authtoken;
-        const email = req.headers.email;
+        const {email, appname: app_name} = req.headers;
 
         if(!md5_device_hash){
             return res.status(401).send("Unauthorized0");
         }
 
         if(!email){
-            return res.status(401).send("Unauthorized1");
+            return res.status(401).send("provide email in header");
+        }
+
+        if(!app_name){
+            return res.status(401).send("provide appname in header");
         }
 
         const users = await UserModel.find({ email });
         if(users.length ===0 ){
-            return res.status(401).send("Unauthorized2");
+            return res.status(401).send("Unauthorized1");
         }
 
-        req.user = {email:users[0].email, md5_device_hash}
+        req.user = {email:users[0].email, md5_device_hash, app_name}
         next();
     }catch(err){
-        return res.status(401).send("Unauthorized3");
+        return res.status(401).send("Unauthorized2");
     }
 }
 
@@ -56,18 +60,18 @@ app.get("/register/:email", async (req, res) => {
     res.status(200).send("OK")
 });
 
-const generateHashKey = (email, md5_device_hash)=>{
-    return `${email}-${md5_device_hash}`
+const generateHashKey = (email, md5_device_hash, app_name)=>{
+    return `${email}-${md5_device_hash}-${app_name}`
 }
 app.get("/api/v1/start-date", verifyAuthToken, async (req, res) => {
     try{
-        const {email, md5_device_hash} = req.user;
-        const record = await PhoneRecord.create({email, md5_device_hash})
+        const {email, md5_device_hash, app_name} = req.user;
+        const record = await PhoneRecord.create({email, md5_device_hash, app_name})
         const client = new Redis(
           "redis://default:xvr8m1Q2Rbx5AuzTgQCTyv0OFIRdynIE@redis-11462.c256.us-east-1-2.ec2.cloud.redislabs.com:11462"
         );
 
-        const redisKey = generateHashKey(email, md5_device_hash);
+        const redisKey = generateHashKey(email, md5_device_hash, app_name);
         await client.set(redisKey, String(record._id));
 
         res.status(200).send("OK");
@@ -81,13 +85,13 @@ app.get("/api/v1/start-date", verifyAuthToken, async (req, res) => {
 
 app.get("/api/v1/stop-date", verifyAuthToken, async (req, res) => {
   try {
-      const {email, md5_device_hash } = req.user;
+      const {email, md5_device_hash, app_name } = req.user;
 
     const client = new Redis(
       "redis://default:xvr8m1Q2Rbx5AuzTgQCTyv0OFIRdynIE@redis-11462.c256.us-east-1-2.ec2.cloud.redislabs.com:11462"
     );
 
-      const redisKey = generateHashKey(email, md5_device_hash);
+      const redisKey = generateHashKey(email, md5_device_hash, app_name);
     const currentlyOpenId = await client.get(redisKey);
 
     await PhoneRecord.updateOne({ _id: currentlyOpenId },{ stop_date: new Date() });
